be514b2cd13744a0ee9d12278e62691c27ee2e3d
PashkovGrigoriy <gripas2006@gmail.com>
2025-11-24 16:52:56 +0500

    Task2

---
+++ Tyuiu.PashkovGV.Sprint4.Task2.V23.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.PashkovGV.Sprint4.Task2.V23.Lib;
+namespace Tyuiu.PashkovGV.Sprint4.Task2.V23.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[] m = { 1,2,3,4,5 };
+            int m2 = 9;
+            var res = ds.Calculate(m);
+            Assert.AreEqual(m2, res);
+        }
+    }
+}
---
+++ Tyuiu.PashkovGV.Sprint4.Task2.V23/Program.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.PashkovGV.Sprint4.Task2.V23.Lib;
+namespace Tyuiu.PashkovGV.Sprint4.Task2.V23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            DataService ds = new DataService();
+            int len;
+            Console.WriteLine("Введите длину ");
+            len = Convert.ToInt32(Console.ReadLine());
+            int[] res = new int[len];
+            for (int i = 0; i < len - 1; i++)
+            {
+                res[i] = rnd.Next(3,8);
+            }
+
+            int m2 = ds.Calculate(res);
+            Console.WriteLine(m2);
+        }
+    }
+}
