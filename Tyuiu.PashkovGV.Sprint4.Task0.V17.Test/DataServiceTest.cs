aa3bec43a3216665655190a356e6fe461fc84876
PashkovGrigoriy <gripas2006@gmail.com>
2025-11-24 15:40:36 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.PashkovGV.Sprint4.Task0.V17.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.PashkovGV.Sprint4.Task0.V17.Lib;
+
+namespace Tyuiu.PashkovGV.Sprint4.Task0.V17.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[] m = { 4, 8, 7, 6, 5, 8, 2, 4, 3, 2 };
+            int m2 = 34;
+            var res = ds.GetSumEvenArrEl(m);
+            Assert.AreEqual(m2, res);
+        }
+    }
+}
---
+++ Tyuiu.PashkovGV.Sprint4.Task0.V17/Program.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.PashkovGV.Sprint4.Task0.V17.Lib;
+
+namespace Tyuiu.PashkovGV.Sprint4.Task0.V17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            int[] m = { 4,8,7,6,5,8,2,4,3,2 };
+            int m2 = ds.GetSumEvenArrEl(m);
+            Console.WriteLine(m2);
+
+        }
+    }
+}
